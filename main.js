bac3e7824067b05738218c8455280a48075d7df0
Audition-CSBlock <coockedmeat@gmail.com>
2018-04-07 16:42:08 +0200

    Added logging (alpha)
    Added logging to a text file, still don't know if it's fully working. So i'm gonna be looking into that

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
-
 const {app, BrowserWindow} = electron;
+// Require electron-log in the app
+var log = require('electron-log');
+var fs = require('fs');
 
 let mainWindow;
 
 // Listen for app to be ready 
 app.on('ready', function(){
+    // Logging
+    log.error();
+    log.debug();
+    log.warn();
+    log.transports.file.level = 'error', 'debug', 'warn';
+    log.transports.file.format = '{h}:{i}:{s}:{ms} {text}';
+    log.transports.file.maxSize = 5 * 1024 * 1024;
+    log.transports.file.file = __dirname + '/log.txt';
+    log.transports.file.streamConfig = { flags: 'w' };
+    log.transports.file.stream = fs.createWriteStream('log.txt');
     //Delete default menu
     electron.app.on('browser-window-created',function(e,window) {
         window.setMenu(null);
     });
     // Create new window
-    mainWindow = new BrowserWindow({width: 1113, height: 600});
+    mainWindow = new BrowserWindow({transparent: true, width: 1113, height: 600});
     // Load html into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
